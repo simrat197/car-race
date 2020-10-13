9e6c2d69f7bc69594226c55c60d13d0be6194cb2
simrat197 <69912389+simrat197@users.noreply.github.com>
2020-10-13 19:59:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var ball;
+var dataBase,position;
+
+function setup(){
+    createCanvas(500,500);
+    dataBase=firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var locofchild=dataBase.ref("Ball/Positions");
+    locofchild.on("value",readop,showerr);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref("Ball/Positions").set({
+        x: ball.x + x,
+        y:  ball.y + y
+    })
+   
+}
+function readop(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+}
+function showerr(){
+    console.log("error");
+}
